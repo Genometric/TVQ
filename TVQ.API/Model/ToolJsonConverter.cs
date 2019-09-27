ffee07690e2cd1be03ed721283c83db3c8946ac8
Vahid Jalili <jalili.vahid@gmail.com>
2019-09-27 13:06:49 -0700

    Update tool deserializer to extract tool ID from bio.tools json.

--- TVQ.API/Model/ToolJsonConverter.cs
+++ TVQ.API/Model/ToolJsonConverter.cs
@@ -22,6 +22,7 @@ namespace Genometric.TVQ.API.Model
                 {"homepage_url", nameof(Tool.Homepage)},
                 {"owner", nameof(Tool.Owner)},
                 {"id", nameof(Tool.IDinRepo)},
+                {"biotoolsID", nameof(Tool.IDinRepo) },
                 {"remote_repository_url", nameof(Tool.CodeRepo)},
                 {"description", nameof(Tool.Description)}
             };
