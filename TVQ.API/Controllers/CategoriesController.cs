5b4d6625a54500b85fd4accd4a3f57143d90ed05
vjalili <jalili.vahid@gmail.com>
2020-01-23 15:00:46 -0800

    Add a controller for Categories.

---
+++ TVQ.API/Controllers/CategoriesController.cs
@@ -0,0 +1,108 @@
+﻿using Genometric.TVQ.API.Infrastructure;
+using Genometric.TVQ.API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Genometric.TVQ.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly TVQContext _context;
+
+        public CategoriesController(TVQContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/v1/Categories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync().ConfigureAwait(false);
+        }
+
+        // GET: api/v1/Categories/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategory(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+
+        // PUT: api/v1/Categories/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(int id, Category category)
+        {
+            if (id != category.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync().ConfigureAwait(false);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/v1/Categories
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Category>> PostCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+
+            return CreatedAtAction("GetCategory", new { id = category.ID }, category);
+        }
+
+        // DELETE: api/v1/Categories/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Category>> DeleteCategory(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+
+            return category;
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _context.Categories.Any(e => e.ID == id);
+        }
+    }
+}
