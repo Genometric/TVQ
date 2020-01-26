06b55291aaca5817651db14226e3cd0f493f6b0b
vjalili <jalili.vahid@gmail.com>
2020-01-26 11:40:56 -0800

    Move all download API endpoints from Repository controller to the Stats controller.

--- TVQ.API/Controllers/RepositoriesController.cs
+++ TVQ.API/Controllers/RepositoriesController.cs
@@ -1,11 +1,8 @@
-﻿using Genometric.TVQ.API.Analysis;
-using Genometric.TVQ.API.Infrastructure;
-using Genometric.TVQ.API.Infrastructure.BackgroundTasks;
+﻿using Genometric.TVQ.API.Infrastructure;
 using Genometric.TVQ.API.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,16 +15,13 @@ namespace Genometric.TVQ.API.Controllers
     {
         private readonly TVQContext _context;
         private readonly ILogger<RepositoriesController> _logger;
-        private readonly IBackgroundTaskQueue _taskQueue;
 
         public RepositoriesController(
             TVQContext context,
-            IBackgroundTaskQueue taskQueue,
             ILogger<RepositoriesController> logger)
         {
             _context = context;
             _logger = logger;
-            _taskQueue = taskQueue;
         }
 
         // GET: api/v1/repositories
@@ -111,157 +105,6 @@ namespace Genometric.TVQ.API.Controllers
             await _context.SaveChangesAsync().ConfigureAwait(false);
 
             return Ok(dataItem);
-        }
-
-        // THIS IS ENDPOINT IS FOR TESTING PURPOSES.
-        [HttpGet("{id}/downloadstats")]
-        public async Task<IActionResult> DownloadStats([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            var repository = QueryRepo(id, true);
-
-            if (repository == null)
-                return NotFound();
-
-            var citations = new Dictionary<int, double[]>();
-            foreach (var association in repository.ToolAssociations)
-            {
-                var tool = association.Tool;
-                foreach (var pub in tool.Publications)
-                {
-                    if (!citations.ContainsKey(tool.ID))
-                        citations.Add(tool.ID, new double[2]);
-
-                    if (pub.Citations != null)
-                        foreach (var citation in pub.Citations)
-                            if (citation.Date < association.DateAddedToRepository)
-                            {
-                                citations[tool.ID][0] += citation.Count;
-                                citations[tool.ID][1] += citation.Count;
-                            }
-                            else
-                            {
-                                citations[tool.ID][1] += citation.Count;
-                            }
-                }
-            }
-
-            var stream = new System.IO.MemoryStream();
-            var writer = new System.IO.StreamWriter(stream);
-            foreach (var item in citations)
-                writer.WriteLine($"{item.Value[0]}\t{item.Value[1]}");
-
-            var contentType = "APPLICATION/octet-stream";
-            var fileName = "TVQStats.csv";
-            stream.Seek(0, System.IO.SeekOrigin.Begin);
-            return File(stream, contentType, fileName);
-        }
-
-        [HttpGet("{id}/downloadstats2")]
-        public async Task<IActionResult> DownloadStats2([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            var repository = QueryRepo(id, true);
-
-            if (repository == null)
-                return NotFound();
-
-            var changes = AnalysisService.GetPrePostCitationCountNormalizedYear(repository);
-            var stream = new System.IO.MemoryStream();
-            var writer = new System.IO.StreamWriter(stream);
-            foreach (var tool in changes)
-                foreach (var change in tool.Value)
-                    writer.WriteLine($"{tool.Key}\t{change.DaysOffset}\t{change.Count}");
-
-            var contentType = "APPLICATION/octet-stream";
-            var fileName = "TVQStats.csv";
-            stream.Seek(0, System.IO.SeekOrigin.Begin);
-            return File(stream, contentType, fileName);
-        }
-
-        // THIS IS AN EXPERIMENTAL ENDPOINT.
-        [HttpGet("{id}/createtime_dist_year")]
-        public async Task<IActionResult> CreateTimeDistYear([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            var repository = QueryRepo(id);
-            var dist = new Dictionary<int, int>();
-            foreach (var association in repository.ToolAssociations)
-            {
-                var year = ((DateTime)association.DateAddedToRepository).Year;
-                if (dist.ContainsKey(year))
-                    dist[year]++;
-                else
-                    dist.Add(year, 1);
-            }
-
-            var distributions = (from record in dist
-                                 select new int[] { record.Key, record.Value }).ToArray();
-
-            return Ok(distributions);
-        }
-
-        // THIS IS AN EXPERIMENTAL ENDPOINT.
-        [HttpGet("{id}/createtime_dist_month")]
-        public async Task<IActionResult> CreateTimeDistMonth([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            var repository = QueryRepo(id);
-            var dist = new Dictionary<string, int>();
-            foreach (var association in repository.ToolAssociations)
-            {
-                var date = ((DateTime)association.DateAddedToRepository);
-                var key = $"{date.Year}-{date.Month}";
-                if (dist.ContainsKey(key))
-                    dist[key]++;
-                else
-                    dist.Add(key, 1);
-            }
-
-            var distributions = (from record in dist
-                                 select new string[] { record.Key, record.Value.ToString() }).ToArray();
-
-            return Ok(distributions);
-        }
-
-        private Repository QueryRepo(int id, bool includeCitations = false)
-        {
-            if (includeCitations)
-            {
-                return
-                    _context.Repositories
-                    .Include(repo => repo.ToolAssociations)
-                        .ThenInclude(x => x.Tool)
-                    .Include(repo => repo.ToolAssociations)
-                        .ThenInclude(x => x.Downloads)
-                    .Include(repo => repo.ToolAssociations)
-                        .ThenInclude(x => x.Tool)
-                            .ThenInclude(x => x.Publications)
-                                .ThenInclude(x => x.Citations)
-                    .Include(repo => repo.Statistics)
-                    .First(x => x.ID == id);
-            }
-            else
-            {
-                return
-                    _context.Repositories
-                    .Include(repo => repo.ToolAssociations)
-                        .ThenInclude(x => x.Tool)
-                    .Include(repo => repo.ToolAssociations)
-                        .ThenInclude(x => x.Downloads)
-                    .Include(repo => repo.ToolAssociations)
-                        .ThenInclude(x => x.Tool)
-                            .ThenInclude(x => x.Publications)
-                    .First(x => x.ID == id);
-            }
         }
 
         private bool RepoExists(int id)
--- TVQ.API/Controllers/StatisticsController.cs
+++ TVQ.API/Controllers/StatisticsController.cs
@@ -1,8 +1,12 @@
-﻿using Genometric.TVQ.API.Infrastructure;
+﻿using Genometric.TVQ.API.Analysis;
+using Genometric.TVQ.API.Infrastructure;
 using Genometric.TVQ.API.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Genometric.TVQ.API.Controllers
@@ -12,6 +16,14 @@ namespace Genometric.TVQ.API.Controllers
     public class StatisticsController : ControllerBase
     {
         private readonly TVQContext _context;
+
+        private enum ReportTypes
+        {
+            BeforeAfterCitationCountPerTool,
+            BeforeAfterCitationCountPerToolNormalizedPerYear,
+            CreateTimeDistributionPerYear,
+            CreateTimeDistributionPerMonth
+        };
 
         public StatisticsController(TVQContext context)
         {
@@ -35,6 +47,172 @@ namespace Genometric.TVQ.API.Controllers
                 return NotFound();
 
             return statistics;
+        }
+
+        // GET: api/v1/Statistics/5/Downloads
+        [HttpGet("{id}/downloads")]
+        public async Task<IActionResult> Downloads([FromRoute] int id, [FromQuery] string type)
+        {
+            if (!Enum.TryParse(type, ignoreCase: true, out ReportTypes reportType))
+            {
+                var builder = new StringBuilder("The required report type is missing from " +
+                                                "the query (e.g., ?type=X); supported values for type are: ");
+                foreach (var t in (ReportTypes[])Enum.GetValues(typeof(ReportTypes)))
+                    builder.Append(t.ToString());
+                return BadRequest(builder.ToString());
+            }
+
+            var statistics = await _context.Statistics.FindAsync(id);
+            if (statistics == null)
+                return NotFound();
+
+            var repository = QueryRepo(statistics.RepositoryID, true);
+            if (repository == null)
+                return NotFound();
+
+            switch (reportType)
+            {
+                case ReportTypes.BeforeAfterCitationCountPerTool:
+                    return BeforeAfterCitationCountPerTool(repository);
+                case ReportTypes.BeforeAfterCitationCountPerToolNormalizedPerYear:
+                    return BeforeAfterCitationCountPerToolNormalizedPerYear(repository);
+                case ReportTypes.CreateTimeDistributionPerYear:
+                    return CreateTimeDistributionPerYear(repository);
+                case ReportTypes.CreateTimeDistributionPerMonth:
+                    return CreateTimeDistributionPerMonth(repository);
+            }
+
+            return BadRequest();
+        }
+
+        private FileStreamResult BeforeAfterCitationCountPerTool(Repository repository)
+        {
+            var citations = new Dictionary<int, double[]>();
+            foreach (var association in repository.ToolAssociations)
+            {
+                var tool = association.Tool;
+                foreach (var pub in tool.Publications)
+                {
+                    if (!citations.ContainsKey(tool.ID))
+                        citations.Add(tool.ID, new double[2]);
+
+                    if (pub.Citations != null)
+                        foreach (var citation in pub.Citations)
+                            if (citation.Date < association.DateAddedToRepository)
+                            {
+                                citations[tool.ID][0] += citation.Count;
+                                citations[tool.ID][1] += citation.Count;
+                            }
+                            else
+                            {
+                                citations[tool.ID][1] += citation.Count;
+                            }
+                }
+            }
+
+            var stream = new System.IO.MemoryStream();
+            var writer = new System.IO.StreamWriter(stream);
+            foreach (var item in citations)
+                writer.WriteLine($"{item.Value[0]}\t{item.Value[1]}");
+
+            var contentType = "APPLICATION/octet-stream";
+            var fileName = "TVQStats.csv";
+            stream.Seek(0, System.IO.SeekOrigin.Begin);
+            return File(stream, contentType, fileName);
+        }
+
+        private FileStreamResult BeforeAfterCitationCountPerToolNormalizedPerYear(Repository repository)
+        {
+            var changes = AnalysisService.GetPrePostCitationCountNormalizedYear(repository);
+            var stream = new System.IO.MemoryStream();
+            var writer = new System.IO.StreamWriter(stream);
+            foreach (var tool in changes)
+                foreach (var change in tool.Value)
+                    writer.WriteLine($"{tool.Key}\t{change.DaysOffset}\t{change.Count}");
+
+            var contentType = "APPLICATION/octet-stream";
+            var fileName = "TVQStats.csv";
+            stream.Seek(0, System.IO.SeekOrigin.Begin);
+            return File(stream, contentType, fileName);
+        }
+
+        private FileStreamResult CreateTimeDistributionPerYear(Repository repository)
+        {
+            var dist = new Dictionary<int, int>();
+            foreach (var association in repository.ToolAssociations)
+            {
+                var year = ((DateTime)association.DateAddedToRepository).Year;
+                if (dist.ContainsKey(year))
+                    dist[year]++;
+                else
+                    dist.Add(year, 1);
+            }
+
+            var stream = new System.IO.MemoryStream();
+            var writer = new System.IO.StreamWriter(stream);
+            foreach (var d in dist)
+                writer.WriteLine($"{d.Key}\t{d.Value}");
+
+            var contentType = "APPLICATION/octet-stream";
+            var fileName = "TVQStats.csv";
+            stream.Seek(0, System.IO.SeekOrigin.Begin);
+            return File(stream, contentType, fileName);
+        }
+
+        private FileStreamResult CreateTimeDistributionPerMonth(Repository repository)
+        {
+            var dist = new Dictionary<string, int>();
+            foreach (var association in repository.ToolAssociations)
+            {
+                var date = ((DateTime)association.DateAddedToRepository);
+                var key = $"{date.Year}-{date.Month}";
+                if (dist.ContainsKey(key))
+                    dist[key]++;
+                else
+                    dist.Add(key, 1);
+            }
+
+            var stream = new System.IO.MemoryStream();
+            var writer = new System.IO.StreamWriter(stream);
+            foreach (var d in dist)
+                writer.WriteLine($"{d.Key}\t{d.Value}");
+
+            var contentType = "APPLICATION/octet-stream";
+            var fileName = "TVQStats.csv";
+            stream.Seek(0, System.IO.SeekOrigin.Begin);
+            return File(stream, contentType, fileName);
+        }
+
+        private Repository QueryRepo(int id, bool includeCitations = false)
+        {
+            if (includeCitations)
+            {
+                return
+                    _context.Repositories
+                    .Include(repo => repo.ToolAssociations)
+                        .ThenInclude(x => x.Tool)
+                    .Include(repo => repo.ToolAssociations)
+                        .ThenInclude(x => x.Downloads)
+                    .Include(repo => repo.ToolAssociations)
+                        .ThenInclude(x => x.Tool)
+                            .ThenInclude(x => x.Publications)
+                                .ThenInclude(x => x.Citations)
+                    .Include(repo => repo.Statistics)
+                    .First(x => x.ID == id);
+            }
+            else
+            {
+                return
+                    _context.Repositories
+                    .Include(repo => repo.ToolAssociations)
+                        .ThenInclude(x => x.Tool)
+                    .Include(repo => repo.ToolAssociations)
+                        .ThenInclude(x => x.Downloads)
+                    .Include(repo => repo.ToolAssociations)
+                        .ThenInclude(x => x.Tool)
+                            .ThenInclude(x => x.Publications)
+                    .First(x => x.ID == id);
+            }
         }
     }
 }
