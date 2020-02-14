52bab6d772d30ef80cf85e503ddd2a45acfee0b4
vjalili <jalili.vahid@gmail.com>
2020-02-13 17:04:17 -0800

    Read the cached bioconda's recipe add date from a file hosted on TVQ's git repo.

--- TVQ.API/Crawlers/ToolRepos/Bioconda.cs
+++ TVQ.API/Crawlers/ToolRepos/Bioconda.cs
@@ -2,8 +2,10 @@
 using Genometric.TVQ.API.Model;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.IO.Compression;
+using System.Net;
 using System.Threading.Tasks;
 using YamlDotNet.RepresentationModel;
 
@@ -11,6 +13,10 @@ namespace Genometric.TVQ.API.Crawlers.ToolRepos
 {
     public class Bioconda : BaseToolRepoCrawler
     {
+        private readonly string _dateAddedFileName = "bioconda_recipes_add_date.txt";
+
+        private Dictionary<string, DateTime?> _addedDates;
+
         public Bioconda(Repository repository,
                         List<Tool> tools,
                         List<Publication> publications,
@@ -23,8 +29,49 @@ namespace Genometric.TVQ.API.Crawlers.ToolRepos
 
         public override async Task ScanAsync()
         {
+            GetAddDate();
             await DownloadFileAsync(Repo.GetURI(), out string archiveFileName).ConfigureAwait(false);
             TraverseArchive(archiveFileName);
+        }
+
+        private void GetAddDate()
+        {
+            /// Why this info is not directly fetched from the Bioconda's repository?
+            /// There is separate script in the Scripts folder that retrieves the date 
+            /// when each tool was added to the Bioconda's repository from the repository's 
+            /// git log. The algorithm to retrieve the dates is not optimized, hence it
+            /// takes significantly long time (in order of hours) to retrieve such info.
+            /// Therefore, until the algorithm is optimized and a better code is implemented 
+            /// here (maybe using LibGit2Sharp (https://github.com/libgit2/libgit2sharp)), 
+            /// it is more practical to run the afore-mentioned script offline, cache the 
+            /// date and store them in the TVQ's git repository, and then read that file 
+            /// in this method (how it is currently implemented).
+
+            var dateAddedFileName = SessionTempPath + Utilities.GetRandomString();
+
+            using var client = new WebClient();
+            client.DownloadFileTaskAsync("https://github.com/Genometric/TVQ/raw/master/data/bioconda/"
+                                         + _dateAddedFileName, dateAddedFileName).Wait();
+
+            _addedDates = new Dictionary<string, DateTime?>();
+
+            using var reader = new StreamReader(dateAddedFileName);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var columns = line.Split('\t');
+                if (!_addedDates.ContainsKey(columns[0]))
+                {
+                    DateTimeOffset.TryParseExact(
+                        columns[1],
+                        "ddd MMM d HH:mm:ss yyyy K",
+                        CultureInfo.InvariantCulture,
+                        DateTimeStyles.None,
+                        out DateTimeOffset dateTimeOffset);
+
+                    _addedDates.Add(columns[0], dateTimeOffset.DateTime);
+                }
+            }
         }
 
         private void TraverseArchive(string archiveFileName)
@@ -47,6 +94,13 @@ namespace Genometric.TVQ.API.Crawlers.ToolRepos
                                 // TODO: log this.
                                 continue;
                             }
+
+                            if (deserializedInfo.ToolRepoAssociation != null &&
+                                deserializedInfo.ToolRepoAssociation.Tool != null &&
+                                deserializedInfo.ToolRepoAssociation.Tool.Name != null &&
+                                _addedDates.ContainsKey(deserializedInfo.ToolRepoAssociation.Tool.Name))
+                                deserializedInfo.ToolRepoAssociation.DateAddedToRepository =
+                                    _addedDates[deserializedInfo.ToolRepoAssociation.Tool.Name];
 
                             if (!TryAddEntities(deserializedInfo))
                             {
