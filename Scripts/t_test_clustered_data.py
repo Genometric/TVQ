4557747293f6159eced95fc5f601061dc0ce0b3d
vjalili <jalili.vahid@gmail.com>
2020-04-02 18:59:36 -0700

    Draft computing between-sample's clusters t-test.

--- Scripts/t_test_clustered_data.py
+++ Scripts/t_test_clustered_data.py
@@ -3,7 +3,7 @@ from numpy import std
 import os
 import sys
 import pandas as pd
-from scipy.stats import ttest_rel, pearsonr
+from scipy.stats import ttest_rel, ttest_ind, pearsonr
 from statistics import mean
 from math import sqrt
 
@@ -17,7 +17,7 @@ def ttest_by_cluster(root, filename):
     clusters = get_clusters(root, filename)
     for k in clusters.groups:
         tools = clusters.get_group(k)
-        (cohen_d, cohen_d_interpretation), (t_statistic, pvalue) = ttest(k, tools)
+        (cohen_d, cohen_d_interpretation), (t_statistic, pvalue) = paired_ttest(tools)
         print(f"\t- Cluster number:\t{k}")
         print(f"\t\t* Tools count:\t{len(tools)}")
         print(f"\t\t* t-Statistic:\t{t_statistic}")
@@ -25,27 +25,9 @@ def ttest_by_cluster(root, filename):
         print(f"\t\t* Cohen's d:\t{cohen_d}\t{cohen_d_interpretation}")
 
 
-def ttest(cluster_label, tools):
-    # columns: a list of all the column headers.
-    # pre:  a list of headers of columns containing normalized citation counts BEFORE a tool was added to the repository.
-    # post: a list of headers of columns containing normalized citation counts AFTER  a tool was added to the repository.
-    columns, pre_headers, post_headers = pre_post_columns(tools)
-
-    # Lists contain citation counts before (pre) and after (post) a tool was added to the repository.
-    pre = []
-    post = []
-    avg_pre = []
-    avg_pst = []
-    for index, row in tools.iterrows():
-        pre_vals = row.get(pre_headers).values.tolist()
-        post_vals = row.get(post_headers).values.tolist()
-
-        pre.append(pre_vals)
-        post.append(post_vals)
-        avg_pre.append(np.average(pre_vals))
-        avg_pst.append(np.average(post_vals))
-
-    return cohen_d(pre_vals,post_vals), ttest_rel(avg_pre, avg_pst)
+def paired_ttest(tools):
+    citations, sums, avg_pre, avg_post = get_vectors(tools)
+    return cohen_d(avg_pre, avg_post), ttest_rel(avg_pre, avg_post)
 
 
 def cohen_d(x,y):
@@ -105,14 +87,68 @@ def get_clusters(root, filename):
     return input_df.groupby(CLUSTER_NAME_COLUMN_LABEL)
 
 
+def get_vectors(tools):
+    # columns: a list of all the column headers.
+    # pre:  a list of headers of columns containing normalized citation counts BEFORE a tool was added to the repository.
+    # post: a list of headers of columns containing normalized citation counts AFTER  a tool was added to the repository.
+    columns, pre_headers, post_headers = pre_post_columns(tools)
+
+    # A list of two-dimensional lists, first dimension is pre counts
+    # and second dimension contains post citation counts.
+    citations = []
+
+    sums = []
+
+    # Lists contain citation counts before (pre) and after (post)
+    # a tool was added to the repository.
+    avg_pre = []
+    avg_pst = []
+    for index, row in tools.iterrows():
+        pre_vals = row.get(pre_headers).values.tolist()
+        post_vals = row.get(post_headers).values.tolist()
+
+        citations.append([pre_vals, post_vals])
+        sums.append(np.sum(pre_vals + post_vals))
+        avg_pre.append(np.average(pre_vals))
+        avg_pst.append(np.average(post_vals))
+
+    return citations, sums, avg_pre, avg_pst
+
+
+def ttest_corresponding_clusters(root, filename_a, filename_b):
+    print(f"\n>>> Processing files: {filename_a} and {filename_b}")
+
+    clusters_a = get_clusters(root, filename_a)
+    clusters_b = get_clusters(root, filename_b)
+
+    for k in clusters_a.groups:
+        _, sums_a, _, _ = get_vectors(clusters_a.get_group(k))
+        _, sums_b, _, _ = get_vectors(clusters_b.get_group(k))
+        t_statistic, pvalue = ttest_ind(sums_a, sums_b, equal_var=False)
+        print(f"\t- Cluster number:\t{k}")
+        print(f"\t\t * t-Statistic:\t{t_statistic}")
+        print(f"\t\t * p-value:\t{pvalue}")
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Missing input path.")
         exit()
 
     inputPath = sys.argv[1]
-    for root, dirpath, filenames in os.walk(inputPath):
-        for filename in filenames:
+
+    filenames = []
+    for root, dirpath, files in os.walk(inputPath):
+        for filename in files:
             if os.path.splitext(filename)[1] == ".csv" and \
                os.path.splitext(filename)[0].endswith(CLUSTERED_FILENAME_POSFIX):
-                ttest_by_cluster(root, filename)
+                filenames.append(filename)
+
+    for filename in filenames:
+        ttest_by_cluster(root, filename)
+
+    # Iterate through all the permutations of repositories,
+    # and compute t-test between corresponding clusters.
+    for i in range(0, len(filenames)-1):
+        for j in range(i+1, len(filenames)):
+            ttest_corresponding_clusters(root, filenames[i], filenames[j])
