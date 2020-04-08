61f98f60951bb7098314d53db373324a59df3821
vjalili <jalili.vahid@gmail.com>
2020-04-07 21:50:48 -0700

    more updates to plotting quartiles.

--- Scripts/plot_cluster_quartiles.py
+++ Scripts/plot_cluster_quartiles.py
@@ -10,6 +10,7 @@ import seaborn as sns
 import itertools
 from scipy.spatial.distance import cdist 
 import sklearn
+from scipy.interpolate import make_interp_spline, BSpline
 
 
 # THIS SCRIPT IS EXPERIMENTAL.
@@ -19,6 +20,10 @@ import sklearn
 CLUSTERED_FILENAME_POSFIX = "_clustered"
 CLUSTER_NAME_COLUMN_LABEL = "cluster_label"
 CLUSTERING_STATS_REPORT_FILENAME = "clustering_stats.txt"
+
+# A list of quartiles to be computed for each column of the 
+# citations matrix (i.e., for each entry of normalized date).
+QUARTILES = [0.25, 0.5, 0.75]
 
 
 def get_clusters(root, filename):
@@ -32,15 +37,10 @@ def get_clusters(root, filename):
 
 
 def get_quartiles(citations):
-    # A list of quartiles to be computed for each column of the 
-    # citations matrix (i.e., for each entry of normalized date).
-    quartiles = [0.25, 0.5, 0.75]
-
     # A data frame whose columns are set to the entries of normalized date,
     # and rows containing their quartiles. The quartile is given as the index 
     # of the row. 
-    return citations.quantile(quartiles)
-
+    return citations.quantile(QUARTILES)
 
 
 def smooth(x, y):
@@ -63,45 +63,66 @@ def plot_smooth_fill_between(ax, x, lower_y, upper_y, color):
     ax.fill_between(s_x, s_lower_y, s_upper_y, facecolor=color, alpha=0.3)
 
 
-def plot(root, filename):
-    before_x, before_median, before_lower_quartile, \
-    before_upper_quartile, before_max, before_min, \
-    after_x, after_median, after_lower_quartile, \
-    after_upper_quartile, after_max, after_min = read_input(os.path.join(root, filename))
+def get_cols(dataframe, row, cols):
+    y = []
+    for x in cols:
+        y.append(dataframe.at[row, x])
+    return y
 
-    fig, ax = plt.subplots()
 
-    plot_smooth_line(ax, before_x, before_median, "red", label="Before adding to repository")
-    plot_smooth_line(ax, before_x, before_max, color="red", linestyle='dashed')
-    plot_smooth_line(ax, before_x, before_min, color="red", linestyle='dotted')
-    plot_smooth_fill_between(ax, before_x, before_lower_quartile, before_upper_quartile, "red")
+def plot(ax, filename, add_legend, quartiles):
+    _, pre_x, post_x = pre_post_columns(quartiles)
 
-    plot_smooth_line(ax, after_x, after_median, "green", label="After adding to repository")
-    plot_smooth_line(ax, after_x, after_max, color="green", linestyle='dashed')
-    plot_smooth_line(ax, after_x, after_min, color="green", linestyle='dotted')
-    plot_smooth_fill_between(ax, after_x, after_lower_quartile, after_upper_quartile, "green")
+    idxes = quartiles.index.sort_values()
+    median = np.median(idxes)
+    for idx in idxes:
+        linestyle = "dashed" if idx == median else "dotted"
+        y = get_cols(quartiles, idx, pre_x)
+        plot_smooth_line(ax, pre_x, y, "red", label="Before adding to repository", linestyle=linestyle)
 
-    start = -1
-    end = 1.01
-    stepsize = 0.4
-    ax.xaxis.set_ticks(np.arange(start, end, stepsize))
-    ax.set_xlabel("Date offset from adding to repository")
+        y = get_cols(quartiles, idx, post_x)
+        plot_smooth_line(ax, post_x, y, "green", label="Before adding to repository", linestyle=linestyle)
+        
+    y_min = get_cols(quartiles, min(idxes), pre_x)
+    y_max = get_cols(quartiles, max(idxes), pre_x)
+    plot_smooth_fill_between(ax, pre_x, y_min, y_max, "red")
 
-    ax.set_ylabel("Citations")
+    y_min = get_cols(quartiles, min(idxes), post_x)
+    y_max = get_cols(quartiles, max(idxes), post_x)
+    plot_smooth_fill_between(ax, post_x, y_min, y_max, "green")
 
-    ax.set_facecolor(BACKGROND_COLOR)
-    ax.legend(loc="upper left")
-    ax.grid(color=GRID_COLOR, linestyle='-', linewidth=1)
-    ax.set_axisbelow(True)
 
-    filename_without_extension = os.path.splitext(filename)[0]
-    plt.title(filename_without_extension)
+    #plot_smooth_line(ax, pre_x, before_median, "red", label="Before adding to repository")
+    #plot_smooth_line(ax, pre_x, before_max, color="red", linestyle='dashed')
+    #plot_smooth_line(ax, pre_x, before_min, color="red", linestyle='dotted')
+    #plot_smooth_fill_between(ax, pre_x, before_lower_quartile, before_upper_quartile, "red")
 
-    image_file = os.path.join(root, filename_without_extension + '.png')
-    if os.path.isfile(image_file):
-        os.remove(image_file)
-    plt.savefig(image_file)
-    plt.close()
+    #plot_smooth_line(ax, post_x, after_median, "green", label="After adding to repository")
+    #plot_smooth_line(ax, post_x, after_max, color="green", linestyle='dashed')
+    #plot_smooth_line(ax, post_x, after_min, color="green", linestyle='dotted')
+    #plot_smooth_fill_between(ax, post_x, after_lower_quartile, after_upper_quartile, "green")
+
+    #start = -1
+    #end = 1.01
+    #stepsize = 0.4
+    #ax.xaxis.set_ticks(np.arange(start, end, stepsize))
+    #ax.set_xlabel("Date offset from adding to repository")
+
+    #ax.set_ylabel("Citations")
+
+    #ax.set_facecolor(BACKGROND_COLOR)
+    #ax.legend(loc="upper left")
+    #ax.grid(color=GRID_COLOR, linestyle='-', linewidth=1)
+    #ax.set_axisbelow(True)
+
+    #filename_without_extension = os.path.splitext(filename)[0]
+    #plt.title(filename_without_extension)
+
+    #image_file = os.path.join(root, filename_without_extension + '.png')
+    #if os.path.isfile(image_file):
+    #    os.remove(image_file)
+    #plt.savefig(image_file)
+    #plt.close()
 
 
 def pre_post_columns(tools):
@@ -157,14 +178,24 @@ def get_vectors(tools):
         avg_pre.append(np.average(pre_vals))
         avg_pst.append(np.average(post_vals))
 
+    # Converts the numeric column headers that are 
+    # represented as strings.
+    indexes = []
+    for col in citations.columns:
+        try:
+            indexes.append(float(col))
+        except ValueError:
+            indexes.append(col)
+    citations.columns = indexes
+
     return citations, pre_post_citations, sums, avg_pre, avg_pst
 
 
-def set_plot_style():
+def set_plot_style(nrows, ncols):
     sns.set()
     sns.set_context("paper")
     sns.set_style("darkgrid")
-    fig, axes = plt.subplots(nrows=4, ncols=2, figsize=(12, 16), dpi=300, gridspec_kw={'width_ratios': [2, 1]})  # , constrained_layout=True)
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(12, 16), dpi=300)  #, gridspec_kw={'width_ratios': [2, 1]})  # , constrained_layout=True)
     plt.subplots_adjust(wspace=0.15, hspace=0.35)
     return fig, axes
 
@@ -176,24 +207,30 @@ if __name__ == "__main__":
 
     inputPath = sys.argv[1]
 
-    fig, ax = set_plot_style()
-    col_counter = 0
+    files = []
     for root, dirpath, filenames in os.walk(inputPath):
         for filename in filenames:
             if os.path.splitext(filename)[1] == ".csv" and \
             os.path.splitext(filename)[0].endswith(CLUSTERED_FILENAME_POSFIX):
-                col_counter += 1
-                filename_without_extension = os.path.splitext(filename)[0]
-                clusters = get_clusters(root, filename)
-                for k in clusters.groups:
-                    citations, _, _, _, _ = get_vectors(clusters.get_group(k))
-                    quartiles = get_quartiles(citations)
+                files.append(filename)
 
+    fig, ax = set_plot_style(len(files), 3)
 
-                
-               
+    row_counter = -1
+    for filename in files:
+        row_counter += 1
+        filename_without_extension = os.path.splitext(filename)[0]
+        clusters = get_clusters(root, filename)
 
-                
+        col_counter = -1
+        for k in clusters.groups:
+            col_counter += 1
+            citations, _, _, _, _ = get_vectors(clusters.get_group(k))
+            quartiles = get_quartiles(citations)
+            plot(ax[row_counter][col_counter], filename_without_extension, True if col_counter == 4 else False, quartiles)
 
-                plot(ax[plot_row], filename_without_extension, True if col_counter == 4 else False, *cluster(root, filename, cluster_count))
-                plot_row += 1
+    image_file = os.path.join(inputPath, 'plot.png')
+    if os.path.isfile(image_file):
+        os.remove(image_file)
+    plt.savefig(image_file, bbox_inches='tight')
+    plt.close()
