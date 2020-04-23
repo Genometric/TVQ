27b4619d0f533b12ddf5a6b1eda1a74ea63707c3
vjalili <jalili.vahid@gmail.com>
2020-04-22 20:00:51 -0700

    Plot a histogram of all clusters together.

--- Scripts/plot_gain_scores.py
+++ Scripts/plot_gain_scores.py
@@ -27,7 +27,7 @@ def get_growthes(tools):
 
 
 def plot(ax, filename, growthes, add_legend, header=None, x_axis_label=None, y_axis_label=None):
-    ax.hist(growthes, density=True, bins=16) # setting density to False will show count, and True will show probability.
+    ax.hist(growthes, density=True, bins=24) # setting density to False will show count, and True will show probability.
     ax.set_yscale('log')
     #ax.yaxis.set_major_formatter(mticker.ScalarFormatter()) # comment this when density=True
     ##ax.yaxis.set_minor_formatter(mticker.ScalarFormatter())
@@ -42,11 +42,11 @@ def plot(ax, filename, growthes, add_legend, header=None, x_axis_label=None, y_a
         ax.set_ylabel(y_axis_label)
 
 
-def set_plot_style(nrows, ncols):
+def set_plot_style(nrows, ncols, fig_height=12, fig_width=12):
     sns.set()
     sns.set_context("paper")
     sns.set_style("darkgrid")
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(12, 12), dpi=300)
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(fig_width, fig_height), dpi=300)
     plt.subplots_adjust(wspace=0.2, hspace=0.2)
     return fig, axes
 
@@ -65,10 +65,10 @@ if __name__ == "__main__":
             os.path.splitext(filename)[0].endswith(CLUSTERED_FILENAME_POSFIX):
                 files.append(filename)
 
-    fig, ax = set_plot_style(len(files), 3)
-
     x_axis_label = "\n Citation Growth"
     y_axis_label = "Probability \n"
+
+    fig, ax = set_plot_style(len(files), 3)
     row_counter = -1
     for filename in files:
         print(f">>> Processing file: {filename}")
@@ -92,6 +92,29 @@ if __name__ == "__main__":
     fig.legend(handles, labels, loc='center', bbox_to_anchor=(0.454, 0.03), ncol=6, framealpha=0.0)
 
     image_file = os.path.join(inputPath, 'gain_scores_clustered.png')
+    if os.path.isfile(image_file):
+        os.remove(image_file)
+    plt.savefig(image_file, bbox_inches='tight')
+    plt.close()
+
+    fig, ax = set_plot_style(1, len(files), fig_height=3, fig_width=16)
+    col_counter = -1
+    for filename in files:
+        print(f">>> Processing file: {filename}")
+        col_counter += 1
+        filename_without_extension = os.path.splitext(filename)[0]
+        repository_name = filename_without_extension.replace(CLUSTERED_FILENAME_POSFIX, "")
+
+        tools = pd.read_csv(os.path.join(root, filename), header=0, sep='\t')
+        growthes = get_growthes(tools)
+        plot(ax[col_counter], filename_without_extension, growthes, True if col_counter == 4 else False, header=repository_name, x_axis_label=x_axis_label, y_axis_label=f"\n {y_axis_label}" if col_counter == 0 else None)           
+        
+    handles, labels = ax[col_counter].get_legend_handles_labels()
+
+    # The "magical" numbers of bbox_to_anchor are determined by trial-and-error.
+    fig.legend(handles, labels, loc='center', bbox_to_anchor=(0.454, 0.03), ncol=6, framealpha=0.0)
+
+    image_file = os.path.join(inputPath, 'gain_scores.png')
     if os.path.isfile(image_file):
         os.remove(image_file)
     plt.savefig(image_file, bbox_inches='tight')
