e5fdba13189249b53ec72630cc603b6cd7f4c050
vjalili <jalili.vahid@gmail.com>
2020-04-29 16:54:15 -0700

    Citation growth histogram formatting.

--- Scripts/citation_growth_histogram.py
+++ Scripts/citation_growth_histogram.py
@@ -15,9 +15,12 @@ from scipy.interpolate import make_interp_spline, BSpline
 from t_test_clustered_data import get_sorted_clusters, get_vectors, get_clusters, CLUSTERED_FILENAME_POSFIX
 from plot_gain_scores import get_growthes
 import matplotlib.ticker as mticker
+from matplotlib.ticker import FormatStrFormatter, PercentFormatter
 
 
 GROWTH_COLUMN_HEADER = "CitationGrowthOnInputData"
+
+COLOR_PALETTES = {"Bioconda": "#3498db", "Bioconductor": "#feb308", "BioTools": "#34495e", "ToolShed": "#41aa33"}
 
 
 def aggregate(input, min, max):
@@ -33,9 +36,12 @@ def aggregate(input, min, max):
     return aggregated
 
 
-def plot(ax, growthes, labels):
-    ax.hist(growthes, label=labels, density=True, bins=16, rwidth=0.6) # setting density to False will show count, and True will show probability.
+def plot(ax, growthes, labels, colors):
+    counts, bins, patches = ax.hist(growthes, label=labels, density=True, bins=12, rwidth=0.65, color = colors, align="left", histtype="bar") # setting density to False will show count, and True will show probability.
     ax.set_yscale('log')
+    ax.set_xticks(bins)
+    ax.xaxis.set_major_formatter(PercentFormatter())
+    ax.set_xlim([-575, 975])
     #ax.yaxis.set_major_formatter(mticker.ScalarFormatter()) # comment this when density=True
     ##ax.yaxis.set_minor_formatter(mticker.ScalarFormatter())
 
@@ -44,7 +50,9 @@ def set_plot_style(nrows, ncols, fig_height=4, fig_width=8):
     sns.set()
     sns.set_context("paper")
     sns.set_style("darkgrid")
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(fig_width, fig_height), dpi=300)
+    flatui = ["#9b59b6", "#3498db", "#e74c3c", "#34495e", "#2ecc71"]
+    sns.palplot(sns.color_palette(flatui))
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(fig_width, fig_height), dpi=600)
     plt.subplots_adjust(wspace=0.2, hspace=0.2)
     return fig, axes
 
@@ -63,7 +71,7 @@ if __name__ == "__main__":
             os.path.splitext(filename)[0].endswith(CLUSTERED_FILENAME_POSFIX):
                 files.append(filename)
 
-    x_axis_label = "\n Citation Growth Percentage"
+    x_axis_label = "\n Citation growth percentage"
     y_axis_label = "Probability \n"
 
     fig, ax = set_plot_style(1, 1)
@@ -71,6 +79,7 @@ if __name__ == "__main__":
     growthes_dict = {}
     all_growthes = []
     labels = []
+    colors = []
     for filename in files:
         print(f">>> Processing file: {filename}")
         row_counter += 1
@@ -79,13 +88,14 @@ if __name__ == "__main__":
         input_df = pd.read_csv(os.path.join(root, filename), header=0, sep='\t')
         all_growthes.append(aggregate(get_growthes(input_df), -500, 1000))
         labels.append(repository_name)
+        colors.append(COLOR_PALETTES[repository_name])
     
-    plot(ax, all_growthes, labels)
+    plot(ax, all_growthes, labels, colors)
 
     ax.set_xlabel(x_axis_label)
     ax.set_ylabel(y_axis_label)
 
-    plt.legend(loc="upper right")
+    plt.legend(loc="upper right", ncol=2)
 
     image_file = os.path.join(inputPath, 'percentage_of_growth.png')
     if os.path.isfile(image_file):
