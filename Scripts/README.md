e4910a1c789f06f77f5bf0c867bb8d4150fad610
vjalili <jalili.vahid@gmail.com>
2020-05-02 12:46:11 -0700

    Add Run script to executed all the post-processing.

--- Scripts/README.md
+++ Scripts/README.md
@@ -1,5 +1,2 @@
 
-1. Run `python .\cluster.py [PATH TO DATA] 3`
-2. Run `python .\plot_cluster_quartiles.py [PATH TO DATA]`
-3. Run `python .\t_test_clustered_data.py [PATH TO DATA]`
-
+Run `python .\run.py [PATH TO DATA] [{TRUE/FALSE} PLOT CITATION CHANGES] [CLUSTER COUNT]`
---
+++ Scripts/run.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import cluster
+import plot_cluster_quartiles
+import t_test_clustered_data
+import citation_growth_histogram
+import plot_gain_scores
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Missing input path.")
+        exit()
+
+    input_path = sys.argv[1]
+
+    plot_changes = False
+    if len(sys.argv) == 3:
+        plot_changes = sys.argv[2]
+
+    if len(sys.argv) == 4:
+        cluster_count = int(sys.argv[3])
+    else:
+        cluster_count = None
+
+    cluster.run(input_path, cluster_count)
+    plot_cluster_quartiles.run(input_path, plot_changes)
+    t_test_clustered_data.run(input_path)
+    citation_growth_histogram.run(input_path)
+    plot_gain_scores.run(input_path)
