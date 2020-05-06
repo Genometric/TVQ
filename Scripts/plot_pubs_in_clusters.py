34cf1f74a995154ee8b0acd673864a58192f07ec
vjalili <VJalili@users.noreply.github.com>
2020-05-05 22:25:19 -0700

    Improve plot.

--- Scripts/plot_pubs_in_clusters.py
+++ Scripts/plot_pubs_in_clusters.py
@@ -11,24 +11,29 @@ import seaborn as sns
 import itertools
 from scipy.spatial.distance import cdist
 import matplotlib.ticker as mticker
-from t_test_clustered_data import get_sorted_clusters, get_vectors, get_clusters, CLUSTERED_FILENAME_POSFIX
+from t_test_clustered_data import get_sorted_clusters, get_vectors, get_clusters, CLUSTERED_FILENAME_POSFIX, get_repo_name
 
 
-def set_plot_style(nrows, ncols, fig_height=12, fig_width=12):
+COLOR_PALETTE = ["#3498db", "#feb308", "#34495e", "#41aa33"]
+
+
+def set_plot_style(nrows, ncols, fig_height=5, fig_width=6):
     sns.set()
     sns.set_context("paper")
     sns.set_style("darkgrid")
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(fig_width, fig_height), dpi=300)
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(fig_width, fig_height), dpi=600)
     plt.subplots_adjust(wspace=0.2, hspace=0.2)
     return fig, axes
 
 
 def run(input_path):
     counts = {}
+    repos = []
     for root, dirpath, filenames in os.walk(input_path):
         for filename in filenames:
             if os.path.splitext(filename)[1] == ".csv" and \
             os.path.splitext(filename)[0].endswith(CLUSTERED_FILENAME_POSFIX):
+                repos.append(get_repo_name(filename))
                 clusters = get_clusters(root, filename)
                 for k in clusters.groups:
                     if k not in counts:
@@ -44,10 +49,17 @@ def run(input_path):
         y = []
         for repo in counts[cluster]:
             y.append(counts[cluster][repo])
-        series.append(ax.bar([j + (width * i) for j in x], y, width, color='r'))
+        series.append(ax.bar([j + (width * i) for j in x], y, width, color=COLOR_PALETTE[i]))
         i += 1
     
-    image_file = os.path.join(input_path, 'counts.png')
+    ax.set_yscale('log')
+    ax.set_ylabel('Count\n')
+    ax.set_xticks([i + width for i in x])
+    ax.set_xticklabels(repos)
+
+    ax.legend(series, ("Cluster " + str(i+1) for i in counts.keys()))
+
+    image_file = os.path.join(input_path, 'num_pubs_in_clusters.png')
     if os.path.isfile(image_file):
         os.remove(image_file)
     plt.savefig(image_file, bbox_inches='tight')
