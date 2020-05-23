618fb0eea9c9545bf069e6850e7ef52601fec75c
vjalili <VJalili@users.noreply.github.com>
2020-05-22 17:20:29 -0700

    Add first test to scripts.

---
+++ Scripts/lib/base.py
@@ -0,0 +1,26 @@
+import os
+
+
+CLUSTERED_FILENAME_POSFIX = "_clustered"
+CLUSTER_NAME_COLUMN_LABEL = "cluster_label"
+
+
+class Base(object):
+    """
+    Base class containing common functionality to be used
+    by the derived types. 
+    """
+
+    def get_repo_name(filename):
+        """
+        Extracts repository name from the given filename.
+
+        :type  filename:    string
+        :param filename:    The filename from which the repository 
+                            name should be extracted.
+
+        :rtype:     string
+        :return:    Repository name.
+        """
+        filename = os.path.basename(filename)
+        return (os.path.splitext(filename)[0]).replace(CLUSTERED_FILENAME_POSFIX, "")
---
+++ Scripts/tests/test_base.py
@@ -0,0 +1,20 @@
+import pytest
+
+from lib.base import Base, CLUSTERED_FILENAME_POSFIX
+
+
+class TestBase(object):
+
+    @pytest.mark.parametrize(
+        "input,expected", 
+        [("name.csv", "name"),
+         ("./a/path/name.csv", "name"),
+         (f"./a/path/name{CLUSTERED_FILENAME_POSFIX}.csv", "name")])
+    def test_get_repo_name(self, input, expected):
+        """
+        Asserts if the `get_repo_name` extracts repository name 
+        from a given filename.
+        """
+
+        # Arrange, Act, Assert
+        assert expected == Base.get_repo_name(input)
