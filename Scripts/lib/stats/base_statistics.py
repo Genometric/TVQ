a598d0921c9229b1a5035fe8a1efdc821d89e850
vjalili <VJalili@users.noreply.github.com>
2020-06-12 12:37:38 -0700

    Add method to get mean of raw citations.

--- Scripts/lib/stats/base_statistics.py
+++ Scripts/lib/stats/base_statistics.py
@@ -6,8 +6,17 @@ from ..base import Base, CLUSTER_NAME_COLUMN_LABEL
 
 import pandas as pd
 
+from statistics import mean
+
+SUM_PRE_CITATIONS_COLUMN_LABEL = "SumPreRawCitations"
+SUM_POST_CITATIONS_COLUMN_LABEL = "SumPostRawCitations"
+
 class BaseStatistics(Base):
     """
     TODO
     """
-    pass
+    
+    @staticmethod
+    def get_mean_of_raw_citations(citations, pre=True):
+        col = SUM_PRE_CITATIONS_COLUMN_LABEL if pre else SUM_POST_CITATIONS_COLUMN_LABEL
+        return mean(citations[col])
