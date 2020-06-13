8f9f75c00bcfd9103f714d25d86359e86b94e118
vjalili <VJalili@users.noreply.github.com>
2020-06-13 12:07:36 -0700

    Implement test.

--- Scripts/tests/stats/test_base_statistics.py
+++ Scripts/tests/stats/test_base_statistics.py
@@ -6,7 +6,7 @@ import pytest
 import os
 import py
 
-from lib.base import Base, CLUSTERED_FILENAME_POSFIX
+from lib.base import Base, CLUSTERED_FILENAME_POSFIX, CLUSTER_NAME_COLUMN_LABEL
 from lib.stats.base_statistics import BaseStatistics
 
 from ..base_test_case import BaseTestCase
@@ -16,4 +16,17 @@ class TestBaseStatistics(BaseTestCase):
     """
     TODO
     """
-    pass
+    
+    def test_get_mean_of_raw_citations(self, test_publications):
+        # Arrange
+        input = test_publications[0]
+        expected_pre = test_publications[1]["avg_sum_pre"]
+        expected_pst = test_publications[1]["avg_sum_post"]
+
+        # Act
+        pre = BaseStatistics.get_mean_of_raw_citations(input, True)
+        pst = BaseStatistics.get_mean_of_raw_citations(input, False)
+        
+        # Assert
+        assert pre == expected_pre
+        assert pst == expected_pst
