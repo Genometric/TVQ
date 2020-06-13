90122d80da3d6f7bac6c84ed2e50daf15384e18d
vjalili <VJalili@users.noreply.github.com>
2020-06-13 12:13:13 -0700

    Add doc string.

--- Scripts/lib/stats/base_statistics.py
+++ Scripts/lib/stats/base_statistics.py
@@ -17,6 +17,20 @@ class BaseStatistics(Base):
     """
     
     @staticmethod
-    def get_mean_of_raw_citations(citations, pre=True):
+    def get_mean_of_raw_citations(publications, pre=True):
+        """
+        Computes the mean of citation counts of all the publications
+        before or after (the `pre` argument) they were added to the repository. 
+
+        :type   publications:   pandas.core.frame.DataFrame
+        :param  publications:   A dataframe containing the publications.
+
+        :type   pre:    boolean
+        :param  pre:    If set to true   or false, respectively returns the mean of  
+                        citations before or after the tools were added to the repository.
+
+        :rtype:     float
+        :return:    The mean of citations.
+        """
         col = SUM_PRE_CITATIONS_COLUMN_LABEL if pre else SUM_POST_CITATIONS_COLUMN_LABEL
-        return mean(citations[col])
+        return mean(publications[col])
