9c239c48892d44c44d5bd160a5b7102d4d232043
vjalili <VJalili@users.noreply.github.com>
2020-07-12 16:05:23 -0700

    Draft a method to plot histogram using the seaborn package.

--- analytics/lib/plot_gain_scores.py
+++ analytics/lib/plot_gain_scores.py
@@ -72,6 +72,52 @@ def plot(ax, filename, growthes, header=None, x_axis_label=None, y_axis_label=No
     if y_axis_label:
         ax.set_ylabel(y_axis_label)
 
+def plot2(ax, growthes, header=None, x_axis_label=None, y_axis_label=None, plot_density=False, hist=True):
+    kde = False if hist else True
+    data = sns.distplot(growthes, bins=25, hist=hist, kde=kde, rug=True, ax=ax, kde_kws={"shade": True})
+
+    if hist:
+        values = [h.get_height() for h in data.patches]
+        values = np.array(values)
+    else:
+        data = data[0].get_data()
+        xvalues = data[0]
+        yvalues = data[1]
+        values = yvalues
+
+    # This is a very hacky solution! 
+    # The objective is to determine if an array should be 
+    # plotted in log scale or not, based on the values. 
+    # For instance, if array contains only few values 
+    # all ~=3, then if plotted in log scale, no ticks 
+    # maybe generated for the y-axis. One way is to 
+    # control minor h-grid lines, and one way is to 
+    # ignore log scale all together. Here we cho0se 
+    # the latter.
+    values_with_no_zero = values.copy()
+    values_with_no_zero = values_with_no_zero[values_with_no_zero != 0]
+    contains_few_values = len(set(values_with_no_zero)) < 3
+
+    if contains_few_values:
+        ax.yaxis.set_major_formatter(ScalarFormatter())
+    elif (contains_few_values or plot_density and max(values) > 0.1) or (not plot_density and max(values) < 10):
+        ax.yaxis.set_major_formatter(ScalarFormatter())
+        ax.yaxis.set_minor_formatter(ScalarFormatter())
+    else:
+        ax.set_yscale('log')
+        if not plot_density:
+            ax.yaxis.set_major_formatter(FormatStrFormatter('%d'))
+
+    if header:
+        ax.set_title(header)
+
+    if x_axis_label:
+        ax.set_xlabel(x_axis_label)
+
+    if y_axis_label:
+        ax.set_ylabel(y_axis_label)
+
+
 
 def set_plot_style(nrows, ncols, fig_height=12, fig_width=12):
     sns.set()
@@ -152,6 +198,31 @@ def run(input_path, plot_density):
     handles, labels = ax[col_counter].get_legend_handles_labels()
 
     image_file = os.path.join(input_path, 'gain_scores.png')
+    if os.path.isfile(image_file):
+        os.remove(image_file)
+    plt.savefig(image_file, bbox_inches='tight')
+    plt.close()
+
+    fig, ax = set_plot_style(1, len(files), fig_height=3, fig_width=16)
+    col_counter = -1
+    for filename in files:
+        col_counter += 1
+        filename_without_extension = os.path.splitext(filename)[0]
+        repository_name = filename_without_extension.replace(CLUSTERED_FILENAME_POSFIX, "")
+
+        tools = pd.read_csv(os.path.join(root, filename), header=0, sep='\t')
+        growthes = get_growthes(tools)
+        plot2(
+            ax[col_counter],
+            growthes,
+            header=repository_name,
+            x_axis_label=x_axis_label,
+            y_axis_label=f"\n {y_axis_label}" if col_counter == 0 else None,
+            plot_density=plot_density)
+
+    handles, labels = ax[col_counter].get_legend_handles_labels()
+
+    image_file = os.path.join(input_path, 'gain_scores_sns.png')
     if os.path.isfile(image_file):
         os.remove(image_file)
     plt.savefig(image_file, bbox_inches='tight')
