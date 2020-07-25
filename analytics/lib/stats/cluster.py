36a3ef8a12961f4f2d35aca43f6fe64b165123c8
vjalili <VJalili@users.noreply.github.com>
2020-07-25 16:18:35 -0700

    Draft plotting clustered data.

--- analytics/lib/stats/cluster.py
+++ analytics/lib/stats/cluster.py
@@ -4,6 +4,7 @@
 
 from .base_statistics import BaseStatistics
 from ..base import Base, CLUSTER_NAME_COLUMN_LABEL, CLUSTERED_FILENAME_POSFIX
+from ..plots.base_plot import BasePlot
 
 import numpy as np
 import os
@@ -16,7 +17,7 @@ import seaborn as sns
 import sklearn
 from matplotlib.lines import Line2D
 
-class Cluster(BaseStatistics):
+class Cluster(BaseStatistics, BasePlot):
     
     def __init__(self):
         self.input_path = None
@@ -35,15 +36,24 @@ class Cluster(BaseStatistics):
         # and initializes it with column headers.
         self._write_cluster_stats()
 
+        # Setup plotting.
+        fig, ax = self.set_plot_style(nrows=4, ncols=2, height=12, width=16, dpi=300)
+        plot_row = 0
+        col_counter = 0
+
         input_files = Base.get_files(self.input_path, include_clustered_files=True)
         for filename in input_files:
-            clustered_publications, stats = self._cluster(filename)
-            self._write_cluster_stats(stats)
-            clustered_publications = self._sort_clusters(clustered_publications)
-            clustered_filename = os.path.join(self.input_path, Base.get_repo_name(filename) + CLUSTERED_FILENAME_POSFIX + '.csv')
-            if os.path.isfile(clustered_filename):
-                os.remove(clustered_filename)
-            clustered_publications.to_csv(clustered_filename, sep='\t', encoding='utf-8', index=False)
+            repo_name = Base.get_repo_name(filename)
+            self._cluster(filename)
+            self._write_cluster_stats(repo_name)
+            self.clustered_publications = self._sort_clusters(self.clustered_publications)
+            clustered_repo_filename = os.path.join(self.input_path, repo_name + CLUSTERED_FILENAME_POSFIX + '.csv')
+            if os.path.isfile(clustered_repo_filename):
+                os.remove(clustered_repo_filename)
+            self.clustered_publications.to_csv(clustered_repo_filename, sep='\t', encoding='utf-8', index=False)
+
+            col_counter += 1
+            self._plot(ax=ax[plot_row], repository_name=repo_name, add_legend=True if col_counter == 4 else False, )
 
     def _cluster(self, filename):
         repo_name = Base.get_repo_name(filename)
@@ -53,31 +63,19 @@ class Cluster(BaseStatistics):
         # Performs hierarchical/agglomerative clustering and 
         # returns the hierarchical clustering encoded as a linkage matrix.
         # The `ward` linkage minimizes the variance of the clusters being merged.
-        linkage_matrix = shc.linkage(citations, method='ward')
+        self.linkage_matrix = shc.linkage(citations, method='ward')
 
-        variance, dist_growth_acceleration, \
-            auto_cluster_count, auto_cut_distance, \
-            manual_cluster_count, manual_cut_distance = \
-            self._get_cluster_count(linkage_matrix, self.cluster_count)
+        self._get_cluster_count(self.linkage_matrix, self.cluster_count)
 
-        _, auto_silhouette_score = self._get_silhouette_score(citations, auto_cluster_count)
-        cluster_labels, manual_silhouette_score = self._get_silhouette_score(citations, manual_cluster_count)
+        _, self.auto_silhouette_score = self._get_silhouette_score(citations, self.auto_cluster_count)
+        cluster_labels, self.manual_silhouette_score = self._get_silhouette_score(citations, self.manual_cluster_count)
 
         # Add cluster information to original data.
         input_df[CLUSTER_NAME_COLUMN_LABEL] = cluster_labels
 
-        formatted_stats =\
-                f"{repo_name}\t" \
-                f"{auto_cluster_count}\t" \
-                f"{auto_cut_distance}\t" \
-                f"{auto_silhouette_score}\t" \
-                f"{manual_cluster_count}\t" \
-                f"{manual_cut_distance}\t" \
-                f"{manual_silhouette_score}\n"
+        self.clustered_publications = input_df
 
-        return input_df, formatted_stats
-
-    def _get_cluster_count(self, linkage_matrix, cluster_count):
+    def _get_cluster_count(self, cluster_count):
         """
         TODO: some tweaks has been made to compute cluster count
         when less than four publications are given. However, 
@@ -87,10 +85,10 @@ class Cluster(BaseStatistics):
         """
         # This method is implemented based on info available from the following link.
         # https://joernhees.de/blog/2015/08/26/scipy-hierarchical-clustering-and-dendrogram-tutorial/#Elbow-Method
-        last = linkage_matrix[-10:, 2]
+        last = self.linkage_matrix[-10:, 2]
         last_rev = last[::-1]
         idxs = np.arange(1, len(last) + 1)
-        variance = pd.DataFrame(last_rev, idxs)
+        self.variance = pd.DataFrame(last_rev, idxs)
 
         # n is the number of times values are differenced. 
         # If zero, the input is returned as-is.
@@ -111,20 +109,18 @@ class Cluster(BaseStatistics):
         acceleration = np.diff(last, n)  
         acceleration_rev = acceleration[::-1]
 
-        dist_growth_acceleration = pd.DataFrame(acceleration_rev, idxs)
+        self.dist_growth_acceleration = pd.DataFrame(acceleration_rev, idxs)
 
-        auto_index = int(acceleration_rev[1:].argmax()) + 3
-        manual_index = auto_index if cluster_count is None else cluster_count
+        self.auto_cluster_count = int(acceleration_rev[1:].argmax()) + 3
+        self.manual_cluster_count = self.auto_cluster_count if cluster_count is None else cluster_count
 
-        if auto_index > len(last_rev):
-            auto_index = len(last_rev)
-        if manual_index > len(last_rev):
-            manual_index = len(last_rev)
+        if self.auto_cluster_count > len(last_rev):
+            self.auto_cluster_count = len(last_rev)
+        if self.manual_cluster_count > len(last_rev):
+            self.manual_cluster_count = len(last_rev)
 
-        return \
-            variance, dist_growth_acceleration, \
-            auto_index, float(last_rev[auto_index - 1]), \
-            manual_index, float(last_rev[manual_index - 1])
+        self.auto_cut_distance = float(last_rev[self.auto_cluster_count - 1])
+        self.manual_cut_distance = float(last_rev[self.manual_cluster_count - 1])
 
     def _get_silhouette_score(self, data, cluster_count):
         # Apply cluster to data.
@@ -157,7 +153,7 @@ class Cluster(BaseStatistics):
 
         return clustered_pubs
 
-    def _write_cluster_stats(self, stats=None):
+    def _write_cluster_stats(self, repo_name=None):
         """
         Writes clustering status to a file. 
 
@@ -168,9 +164,16 @@ class Cluster(BaseStatistics):
                         If given, the stats are written to clustering stats file.
         """
         filename = os.path.join(self.input_path, self.clustering_stats_filename)
-        if stats:
+        if repo_name:
             with open(filename, "a") as f:
-                f.write(stats)
+                f.write(\
+                    f"{repo_name}\t" \
+                    f"{self.auto_cluster_count}\t" \
+                    f"{self.auto_cut_distance}\t" \
+                    f"{auto_silhouette_score}\t" \
+                    f"{self.manual_cluster_count}\t" \
+                    f"{self.manual_cut_distance}\t" \
+                    f"{self.manual_silhouette_score}\n")
         else:
             with open(filename, "w") as f:
                 f.write(
@@ -181,3 +184,53 @@ class Cluster(BaseStatistics):
                     "Manually-set Cluster Count\t" \
                     "Manually-set Dendrogram Cut Height\t" \
                     "Manually-set Cluster Silhouette Score\n")
+
+    def _plot(self, ax, repository_name, add_legend):
+        col0 = ax[0]
+        col1 = ax[1]
+
+        auto_cut_color = "orange"
+        manu_cut_color = "orange"
+
+        auto_cut_line_style = "dotted"
+        manu_cut_line_style = "dashed"
+
+        linewidth = 1.5
+
+        # Plots the hierarchical clustering as a dendrogram.
+        dend = shc.dendrogram(self.linkage_matrix, no_labels=True, orientation="right", ax=col0)  #, truncate_mode="level")
+        col0.axvline(x=self.auto_cut_distance, color=auto_cut_color, linewidth=linewidth, linestyle=auto_cut_line_style)
+        col0.axvline(x=self.manual_cut_distance, color=manu_cut_color, linewidth=linewidth, linestyle=manu_cut_line_style)
+
+        # Plot to a PNG file.
+        col0.set_title(repository_name)
+        col0.set_xlabel("Height")
+        col0.grid(axis='x', which='major', color='w')
+
+        col0.text(\
+            0.82, 0.1, \
+            "Silhouette Score={:.4f}".format(manual_silhouette_score), \
+            horizontalalignment='center', \
+            verticalalignment='center', \
+            transform=col0.transAxes)
+
+        # Plot the Elbow method's results.
+        col1.plot(self.variance, label="Variance", marker='o', color='green')
+        col1.plot(self.dist_growth_acceleration, label="Distance growth acceleration", marker="x", color="blue")
+
+        col1.set_title(repository_name)
+        col1.set_xlabel("Number of clusters")
+        col1.set_ylabel("Distortion")
+
+        if add_legend:
+            col1.legend(loc='center', bbox_to_anchor=(0.5, -0.3), framealpha=0.0, fancybox=True)
+
+            lines = [\
+                Line2D([0], [0], color=auto_cut_color, linewidth=linewidth, linestyle=auto_cut_line_style), \
+                Line2D([0], [0], color=manu_cut_color, linewidth=linewidth, linestyle=manu_cut_line_style)]
+
+            labels = ['Auto-determined cut height', 'Manually-set cut height']
+            col0.legend(lines, labels, loc='center', bbox_to_anchor=(0.5, -0.3), framealpha=0.0, fancybox=True)
+
+        col1.axvline(x=self.auto_cluster_count, color=auto_cut_color, linewidth=1.5, linestyle=auto_cut_line_style)
+        col1.axvline(x=self.manual_cluster_count, color=manu_cut_color, linewidth=1.5, linestyle=manu_cut_line_style)
