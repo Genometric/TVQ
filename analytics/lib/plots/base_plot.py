4a191ee48cb12343f221ea8c51bded03ebba4c91
vjalili <VJalili@users.noreply.github.com>
2020-07-24 21:00:30 -0700

    Draft BasePlot.

---
+++ analytics/lib/plots/base_plot.py
@@ -0,0 +1,36 @@
+"""
+
+"""
+
+import numpy as np
+import os
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.cluster.hierarchy as shc
+from sklearn.cluster import AgglomerativeClustering
+import seaborn as sns
+import sklearn
+from matplotlib.lines import Line2D
+
+
+class BasePlot(object):
+    """description of class"""
+
+    def __init__(self):
+        pass
+
+    def set_plot_style(nrows, ncols, width=None, height=None, wspace=0.15, hspace=0.35, dpi=300, sharex=False, width_ratios=[2, 1]):
+        sns.set()
+        sns.set_context("paper")
+        sns.set_style("darkgrid")
+
+        if not height:
+            height = 3 * nrows
+
+        if not width:
+            width = 4 * ncols
+
+        fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(height, width), dpi=dpi, sharex=sharex, gridspec_kw={'width_ratios':width_ratios})
+        plt.subplots_adjust(wspace=wspace, hspace=hspace)
+        return fig, axes
