dfb0e40dd07b86ec6bd44e119d704f2ebeea299f
VJalili <VJalili@users.noreply.github.com>
2021-02-22 20:15:33 -0800

    Improve registration and usage of the logger.

--- Crawlers/ToolShed/ToolShedCrawler/Crawler.cs
+++ Crawlers/ToolShed/ToolShedCrawler/Crawler.cs
@@ -2,7 +2,6 @@
 using Genometric.TVQ.WebService.Model;
 using Genometric.TVQ.WebService.Model.Associations;
 using Genometric.TVQ.WebService.Model.JsonConverters;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
@@ -117,45 +116,48 @@ namespace ToolShedCrawler
             HttpResponseMessage response = client.GetAsync(new Uri(_uri + _categoriesEndpoint)).GetAwaiter().GetResult();
             string content;
             if (response.IsSuccessStatusCode)
+            {
                 content = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            }
             else
-                /// TODO: replace with an exception.
+            {
+                Logger.LogCritical($"Failed getting categories from ToolShed; reason: {response.ReasonPhrase}");
                 return;
+            }
 
-            Logger.LogDebug("Received Categories from ToolShed, deserializing them.");
-            var associations = JsonConvert.DeserializeObject<List<CategoryRepoAssociation>>(
-                content, CategoryJsonSerializerSettings);
-
+            Logger.LogDebug($"Serializing the received Categories to `{CatogiresFilename}`");
             WriteToJson(content, CatogiresFilename);
+            Logger.LogInformation($"Serialized Categories to `{CatogiresFilename}`");
         }
 
         private async Task<List<Tool>> GetTools()
         {
+            Logger.LogDebug("Getting Tools list from ToolShed.");
             using var client = new HttpClient();
             HttpResponseMessage response = await client.GetAsync(new Uri(_uri + _repositoriesEndpoint)).ConfigureAwait(false);
             string content;
             if (response.IsSuccessStatusCode)
+            {
                 content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            }
             else
-                /// TODO: replace with an exception.
+            {
+                Logger.LogCritical($"Failed getting categories from ToolShed; reason: {response.ReasonPhrase}");
                 return null;
+            }
 
+            Logger.LogDebug($"Serializing the received Tools to `{ToolsFilename}`");
             WriteToJson(content, ToolsFilename);
             File.WriteAllText(ToolsFilename, content);
+            Logger.LogInformation($"Serialized Tools to `{ToolsFilename}`");
 
+            Logger.LogDebug($"Creating staging area per tool in: {SessionTempPath}");
             var tools = JsonConvert.DeserializeObject<List<Tool>>(content);
             foreach (var tool in tools)
                 tool.EnsureStagingArea(SessionTempPath);
+            Logger.LogDebug($"Created staging areas.");
 
             return tools;
-        }
-
-        private void WriteToJson(string content, string filename)
-        {
-            var json = JsonConvert.DeserializeObject<object>(content, SerializerSettings);
-            using StreamWriter writer = new StreamWriter(filename);
-            var serializer = JsonSerializer.Create(SerializerSettings);
-            serializer.Serialize(writer, json);
         }
 
         /// <summary>
@@ -164,6 +166,7 @@ namespace ToolShedCrawler
         /// </summary>
         private void GetPublications(List<Tool> tools)
         {
+            Logger.LogDebug("Setting up TPL for downloading tools archives.");
             var linkOptions = new DataflowLinkOptions
             {
                 PropagateCompletion = true
@@ -185,48 +188,59 @@ namespace ToolShedCrawler
             extractXMLs.LinkTo(extractPublications, linkOptions);
             extractPublications.LinkTo(cleanup, linkOptions);
 
+            Logger.LogDebug($"Starting to process (download archive, extract XML, extract citation) {tools.Count} tools *asynchronously*.");
+            int t = 0;
             foreach (var info in tools)
+            {
+                t++;
+                if (t > 3)
+                    break;
                 downloader.Post(info);
+            }
 
             downloader.Complete();
 
             cleanup.Completion.Wait();
 
+            Logger.LogDebug($"Serializing extract bibliographies to `{PublicationsFilename}`");
             WriteToJson(JsonConvert.SerializeObject(Publications, SerializerSettings), PublicationsFilename);
+            Logger.LogDebug($"Serialized extracted bibliographies.");
         }
 
-        private Tool Downloader(Tool info)
+        private Tool Downloader(Tool tool)
         {
             try
             {
+                Logger.LogDebug($"Downloading archive of `{tool.Name}`.");
+
                 /// Note: do not use base WebClient, because it cannot 
                 /// download multiple files concurrently.
                 using var client = new WebClient();
                 client.DownloadFile(
                     address: new Uri(
                         $"https://toolshed.g2.bx.psu.edu/repos/" +
-                        $"{info.Owner}/{info.Name}/" +
+                        $"{tool.Owner}/{tool.Name}/" +
                         $"archive/tip.zip"),
-                    fileName: info.ArchiveFilename);
-                Logger.LogDebug($"Successfully downloaded archive of {info.Name}.");
-                return info;
+                    fileName: tool.ArchiveFilename);
+                Logger.LogDebug($"Successfully downloaded archive of `{tool.Name}`.");
+                return tool;
             }
             catch (WebException e)
             {
-                Logger.LogDebug($"Failed downloading archive of {info.Name}: {e.Message}");
+                Logger.LogDebug($"Failed downloading archive of `{tool.Name}`: {e.Message}");
                 return null;
             }
         }
 
-        private Tool WrapperExtractor(Tool info)
+        private Tool WrapperExtractor(Tool tool)
         {
-            if (info == null)
+            if (tool == null)
                 return null;
 
             try
             {
-                //Logger.LogDebug($"Extracting XML files from tool {info.ToolRepoAssociation.Tool.Name} archive.");
-                using ZipArchive archive = ZipFile.Open(info.ArchiveFilename, ZipArchiveMode.Read);
+                Logger.LogDebug($"Extracting XML files from tool `{tool.Name}` archive.");
+                using ZipArchive archive = ZipFile.Open(tool.ArchiveFilename, ZipArchiveMode.Read);
                 foreach (ZipArchiveEntry entry in archive.Entries)
                     if (entry.FullName.EndsWith(".xml", StringComparison.OrdinalIgnoreCase))
                     {
@@ -234,7 +248,7 @@ namespace ToolShedCrawler
                         /// collision when extracting and storing files with common names
                         /// in a common folder, which organized under different folders in 
                         /// an archive.
-                        var extractedFileName = info.ArchiveExtractionPath +
+                        var extractedFileName = tool.ArchiveExtractionPath +
                             Path.GetFileNameWithoutExtension(entry.FullName) + Utilities.GetRandomString(8);
 
                         /// Surrounding the file extraction from archive in a
@@ -244,25 +258,25 @@ namespace ToolShedCrawler
                         /// may be valid.
                         try
                         {
-                            //Logger.LogInformation($"Extracting XML file {entry.FullName} of tool {info.ToolRepoAssociation.Tool.Name}.");
+                            Logger.LogInformation($"Extracting XML file `{entry.FullName}` of tool `{tool.Name}`.");
                             entry.ExtractToFile(extractedFileName);
-                            //Logger.LogInformation($"Successfully extracted XML file {entry.FullName} of tool {info.ToolRepoAssociation.Tool.Name}.");
-                            info.XMLFiles.Add(extractedFileName);
+                            Logger.LogInformation($"Successfully extracted XML file `{entry.FullName}` of tool `{tool.Name}`.");
+                            tool.XMLFiles.Add(extractedFileName);
                         }
                         catch (InvalidDataException e)
                         {
                             // This exception is thrown when the Zip archive cannot be read.
-                            //Logger.LogDebug($"Failed extracting XML file {entry.FullName} of tool {info.ToolRepoAssociation.Tool.Name}: {e.Message}");
+                            Logger.LogDebug($"Failed extracting XML file `{entry.FullName}` of tool `{tool.Name}`: {e.Message}");
                         }
                     }
 
-                //Logger.LogDebug($"Extracted {info.XMLFiles.Count} XML file(s) for tool {info.ToolRepoAssociation.Tool.Name}.");
-                return info;
+                Logger.LogDebug($"Extracted `{tool.XMLFiles.Count}` XML file(s) for tool `{tool.Name}`.");
+                return tool;
             }
             catch (InvalidDataException e)
             {
                 // This exception is thrown when the Zip archive cannot be read.
-                //Logger.LogDebug($"Failed extracting XML files from tool {info.ToolRepoAssociation.Tool.Name} archive: {e.Message}");
+                Logger.LogDebug($"Failed extracting XML files from tool `{tool.Name}` archive: {e.Message}");
                 return null;
             }
         }
@@ -276,8 +290,8 @@ namespace ToolShedCrawler
             {
                 Logger.LogDebug(
                     $"Extracting publication info from XML file " +
-                    $"{Path.GetFileNameWithoutExtension(filename)} " +
-                    $"of tool {info.Name}.");
+                    $"`{Path.GetFileNameWithoutExtension(filename)}` " +
+                    $"of tool `{info.Name}`.");
 
                 try
                 {
@@ -301,15 +315,13 @@ namespace ToolShedCrawler
                                     try
                                     {
                                         if (TryParseBibitem(item.Value, out Publication pub))
-                                        {
                                             Publications[info.ID].Add(pub);
-                                        }
                                     }
                                     catch (ArgumentException e)
                                     {
                                         Logger.LogDebug(
                                             $"Error extracting publication from XML file of tool " +
-                                            $"{info.Name}:{e.Message}");
+                                            $"`{info.Name}`:{e.Message}");
                                     }
                                     break;
                             }
@@ -317,8 +329,8 @@ namespace ToolShedCrawler
 
                     Logger.LogDebug(
                         $"Successfully extract publication info from XML file " +
-                        $"{Path.GetFileNameWithoutExtension(filename)} " +
-                        $"of tool {info.Name}.");
+                        $"`{Path.GetFileNameWithoutExtension(filename)}` " +
+                        $"of tool `{info.Name}`.");
                 }
                 catch (System.Xml.XmlException e)
                 {
@@ -326,8 +338,8 @@ namespace ToolShedCrawler
                     /// file has multiple roots.
                     Logger.LogDebug(
                          $"Failed extracting publication info from XML file " +
-                         $"{Path.GetFileNameWithoutExtension(filename)}" +
-                         $" of tool {info.Name}: {e.Message}");
+                         $"`{Path.GetFileNameWithoutExtension(filename)}`" +
+                         $" of tool `{info.Name}`: {e.Message}");
 
                     return null;
                 }
@@ -349,6 +361,14 @@ namespace ToolShedCrawler
                 publication = null;
                 return false;
             }
+        }
+
+        private void WriteToJson(string content, string filename)
+        {
+            var json = JsonConvert.DeserializeObject<object>(content, SerializerSettings);
+            using StreamWriter writer = new StreamWriter(filename);
+            var serializer = JsonSerializer.Create(SerializerSettings);
+            serializer.Serialize(writer, json);
         }
 
         private void Cleanup(Tool info)
--- Crawlers/ToolShed/ToolShedCrawler/Program.cs
+++ Crawlers/ToolShed/ToolShedCrawler/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+using System;
 
 namespace ToolShedCrawler
 {
@@ -7,16 +11,51 @@ namespace ToolShedCrawler
     {
         static void Main(string[] args)
         {
-            var serviceCollection = new ServiceCollection();
-            ConfigureServices(serviceCollection);
-            var serviceProvider = serviceCollection.BuildServiceProvider();
-            var crawler = serviceProvider.GetService<Crawler>();
-            crawler.Crawl();
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            try
+            {
+                var serviceProvider = services.BuildServiceProvider();
+                var crawler = serviceProvider.GetService<Crawler>();
+                crawler.Crawl();
+                Log.Debug("Crawler finished crawling successfully.");
+            }
+            catch(Exception e)
+            {
+                Log.Fatal(e, "Program terminated unexpectedly!");
+            }
+            finally
+            {   
+                Log.CloseAndFlush();
+            }
         }
 
         private static void ConfigureServices(IServiceCollection services)
         {
-            services.AddLogging(configure => configure.AddConsole()).AddTransient<Crawler>();
+            // Build configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            // Add logging
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddSerilog();
+            });
+
+            // Initialize serilog logger
+            Log.Logger = new LoggerConfiguration()
+                 .MinimumLevel.Debug()
+                 .Enrich.FromLogContext()
+                 .WriteTo.File("./log_file.log", LogEventLevel.Verbose)
+                 .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+                 .CreateLogger();
+
+            // Add access to generic IConfigurationRoot
+            services.AddSingleton(configuration);
+            services.AddTransient<Crawler>();
         }
     }
 }
