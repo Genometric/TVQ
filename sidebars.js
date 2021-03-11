55b610297234826aae93da8fce9d6c60ef8a4c76
vjalili <VJalili@users.noreply.github.com>
2021-03-10 17:34:35 -0800

    Add docs for the repo crawling jobs.

--- sidebars.js
+++ sidebars.js
@@ -3,7 +3,7 @@ module.exports = {
     'About': ['about/intro', 'about/overview'],
 	'Quick Start': ['getting_started/quickstart'],
 	'Analytics': ['analytics/about'],
-	'Web Service': ['webservice/about'],
+	'Web Service': ['webservice/about', 'webservice/repo'],
 	'Offline Crawlers': ['offline_crawlers/about'],
   },
 };
