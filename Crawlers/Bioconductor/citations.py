a474524814f5ece0a23820d0b04191249e46f057
vjalili <VJalili@users.noreply.github.com>
2021-03-25 01:49:45 -0700

    Add few prints.

--- Crawlers/Bioconductor/citations.py
+++ Crawlers/Bioconductor/citations.py
@@ -43,6 +43,20 @@ def get_bibitem(filename):
         return bibitem
 
 
+def uninstall_package(package_name):
+    """
+    It basically removes the folder to which the package
+    is installed.Sometimes get permission denied error for
+    some folders when deleting them; hence, first try to
+    ensure the write permission for every file and folder.
+    """
+    package_dir = os.path.join(R_LIB_DIRECTORY, package_name)
+    for root, _, files in os.walk(package_dir):
+        for filename in files:
+            os.chmod(os.path.join(root, filename), stat.S_IWUSR)
+    shutil.rmtree(package_dir)
+
+
 def get_citation(package_name):
     """
     The overall process:
@@ -59,38 +73,44 @@ def get_citation(package_name):
     # Clone the repository of a given package to a temporary path.
     git_clone_dir = os.path.join(tempfile.mkdtemp(prefix="tvq_"), package_name)
     try:
+        print("\tcloning...", end="")
         subprocess.check_output(
             f"git clone https://git.bioconductor.org/packages/{package_name} {git_clone_dir}",
             shell=True, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
+    except subprocess.CalledProcessError:
+        print("failed, skipping.")
         return None
+    print("done", end="")
 
     # Copying the cloned repository to R's library path
     # is equivalent of installing the package. Without
     # this step, the script used to get citations do not
     # recognize the package as installed.
     try:
+        print("\tinstalling...", end="")
         shutil.move(git_clone_dir, R_LIB_DIRECTORY)
     except shutil.Error:
-        pass
+        uninstall_package(package_name)
+        try:
+            shutil.move(git_clone_dir, R_LIB_DIRECTORY)
+        except shutil.Error:
+            print("failed, skipping.")
+            return None
+    print("done", end="")
 
     # Calls the get_package_citation script which
     # gets the citation of a given package and
     # persists it in the given filename.
+    print("\textracting citation...", end="")
     tmp_citation_filename = os.path.join(".", "tmp.txt")
     subprocess.run(f"{R_SCRIPT_PATH} .\get_package_citation.R {package_name} {tmp_citation_filename}")
 
     # Read the BibItem the R script outputs.
     bibitem = get_bibitem(tmp_citation_filename)
+    print("done", end="")
 
     # Clean up.
-    # Sometimes get permission denied error for some folders when deleting them;
-    # hence, first try to ensure the write permission for every file and folder.
-    package_dir = os.path.join(R_LIB_DIRECTORY, package_name)
-    for root, _, files in os.walk(package_dir):
-        for filename in files:
-            os.chmod(os.path.join(root, filename), stat.S_IWUSR)
-    shutil.rmtree(package_dir)
+    uninstall_package(package_name)
     os.remove(tmp_citation_filename)
 
     return bibitem
@@ -100,15 +120,22 @@ if __name__ == "__main__":
     packages = get_package_names()
     citations = {}
     unsuccessful_packages = []
+    print("Getting citation information ...")
+    i = 0
     for package in packages:
+        i += 1
+        print(f"[{i:02d}/{len(packages)}] {package}:\t", end="")
         bibitem = get_citation(package)
         if bibitem:
             citations[package] = bibitem
+            print("\trecorded.")
         else:
             unsuccessful_packages.append(package)
 
+    print(f"Writing citations to {CITATIONS_FILENAME} ...", end="")
     with open(CITATIONS_FILENAME, "w") as f:
         json.dump(citations, f, indent="\t")
+    print("done.")
 
     with open("unsuccessful_packages.json", "w") as f:
         json.dump(unsuccessful_packages, f, indent="\t")
